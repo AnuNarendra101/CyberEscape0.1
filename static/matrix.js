94531335c02c86d651ac0bfa14ae46a464a74d5e
Anu Narendra N C <anuncnath5510@gmail.com>
2026-08-08 10:53:43 +0530

    Update matrix.js

--- static/matrix.js
+++ static/matrix.js
@@ -21,7 +21,7 @@ function draw(){
     ctx.fillStyle = "rgba(0,0,0,0.05)";
     ctx.fillRect(0,0,canvas.width,canvas.height);
 
-   ctx.fillStyle = "#ff1a1a";
+   ctx.fillStyle = "#ff0000";
     ctx.font = fontSize + "px monospace";
 
     for(let i = 0; i < drops.length; i++){
